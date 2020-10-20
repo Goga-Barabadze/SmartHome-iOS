d0c25ae35d939ce6bb1b9f434d50fa386664521a
Goga Barabadze <goga.barabadze73@gmail.com>
2020-10-20 15:10:24 +0200

    Reinstalled Pods

--- Pods/Target Support Files/Pods-SmartHomeTests/Pods-SmartHomeTests-umbrella.h
+++ Pods/Target Support Files/Pods-SmartHomeTests/Pods-SmartHomeTests-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_Smart_HomeTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Smart_HomeTestsVersionString[];
+FOUNDATION_EXPORT double Pods_SmartHomeTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SmartHomeTestsVersionString[];
 
